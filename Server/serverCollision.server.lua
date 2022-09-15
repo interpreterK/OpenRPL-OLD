8eb88a11d21cd00619e4acbf592556cc23f0d095
interpreterK <rhpidfyre@protonmail.com>
2022-09-15 12:40:53 -0400

    [Push] Better serverCollision code, not all BaseParts work yet, only a selected few.

--- Server/serverCollision.server.lua
+++ Server/serverCollision.server.lua
@@ -10,41 +10,46 @@ PhysicsList_Invoke.OnServerInvoke = function(_)
 	return PhysicsList
 end
 
-local function ConditionPassable(i)
-	return i.Anchored and i.CanCollide and i.ClassName~="Terrain"
+local Supported_Objects = {
+	"Part",
+	"TrussPart"
+}
+local function ConditionPassable(inst)
+	if inst:IsA("BasePart") and inst.Anchored and inst.CanCollide then
+		for i = 1, #Supported_Objects do
+			if inst.ClassName == Supported_Objects[i] then
+				return true
+			end
+		end
+	end
+	return false
 end
 workspace.DescendantAdded:Connect(function(descendant)
-	if descendant:IsA('BasePart') then
-		if ConditionPassable(descendant) then
-			insert(PhysicsList, descendant)
-			descendant.CanCollide = false
-		else
-			insert(IgnoredPhysicsList, descendant)
-		end
+	if ConditionPassable(descendant) then
+		insert(PhysicsList, descendant)
+		descendant.CanCollide = false
+	else
+		insert(IgnoredPhysicsList, descendant)
 	end
 end)
 workspace.DescendantRemoving:Connect(function(descendant)
-	if descendant:IsA('BasePart') then
-		local f = find(PhysicsList, descendant)
-		if f then
-			remove(PhysicsList, f)
-		end
+	local f = find(PhysicsList, descendant)
+	if f then
+		remove(PhysicsList, f)
 	end
 end)
 
 local desc = workspace:GetDescendants()
 for i = 1, #desc do
 	local p = desc[i]
-	if p:IsA("BasePart") then
-		if ConditionPassable(p) then
-			local f = find(PhysicsList, p)
-			if not f then
-				insert(PhysicsList, p)
-				p.CanCollide = false
-			end
-		else
-			insert(IgnoredPhysicsList, p)
+	if ConditionPassable(p) then
+		local f = find(PhysicsList, p)
+		if not f then
+			insert(PhysicsList, p)
+			p.CanCollide = false
 		end
+	else
+		insert(IgnoredPhysicsList, p)
 	end
 end
 
