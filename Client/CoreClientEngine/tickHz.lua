b9112a8b14b79539510af08b6e897f538074b23c
interpreterK <rhpidfyre@protonmail.com>
2022-09-21 12:41:05 -0400

    [Commit] tickHz optimizations

--- Client/CoreClientEngine/tickHz.lua
+++ Client/CoreClientEngine/tickHz.lua
@@ -2,18 +2,18 @@ local tickHz = {}
 tickHz.__index = tickHz
 
 local Modules = _G.__phys_modules__
-local Common = Modules.Common
-local S, New = Common.S, Common.New
+local Common  = Modules.Common
+local S, New  = Common.S, Common.New
 
 local RunS = S.RunService
 
 local function CreateVM(Hz, Step_Func, STEP)
     local Hz_Bind = New('BindableEvent')
-    local tdt = 0
+    local tdt     = 0
+    local Connection;
     Hz = Hz or 60
-    if STEP then
-        Hz=Hz/2
-    end
+    if STEP then Hz=Hz/2 end
+
     local function HzControl(dt,st)
         if Hz == 0 then
             Hz_Bind:Fire(tdt,dt,st)
@@ -25,26 +25,26 @@ local function CreateVM(Hz, Step_Func, STEP)
             tdt=0
         end
     end
-    local Connection;
     if Step_Func == "Stepped" then
         Connection = RunS.Stepped:Connect(function(st,dt)
-            HzControl(dt,st,true)
+            HzControl(dt,st)
         end)
     else
         Connection = RunS[Step_Func]:Connect(HzControl)
     end
+
     return {
-        Hz_Bind = Hz_Bind,
+        Hz_Bind    = Hz_Bind,
         Connection = Connection
     }
 end
 
 function tickHz.new(Hz, Step)
     assert(Step, "Step instruction is required")
-    local self = {}
-    local VM = CreateVM(Hz, Step)
-    self.Hz = Hz
-    self.TickStep = VM.Hz_Bind.Event
+    local self          = {}
+    local VM            = CreateVM(Hz, Step)
+    self.Hz             = Hz
+    self.TickStep       = VM.Hz_Bind.Event
     self.TickConnection = VM.Connection
     return setmetatable(self, tickHz)
 end
